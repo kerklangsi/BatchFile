63eef455ae6e613ffacc36f3c1e232a43efd1b27
kerklangsi <arifilah.01@gmail.com>
2025-09-18 13:09:52 +0800

    Update and rename Ffmpeg_Tool.bat to Ffmpeg_Tool.cmd

--- Ffmpeg_Tool/Ffmpeg_Tool.cmd
+++ Ffmpeg_Tool/Ffmpeg_Tool.cmd
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCqDJGmW+0UiKRYUag2OOXiGKbsZzPry+e+7hkIeQe86dpvI5pOHNMgG6HnGdIIu3lxTlM4wJRVLahOnYk8hp2dRomWJO4e6tQLxT0/J0kQ/F1lSoW7AiSx1adBn+g==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJhZks0
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
-::ZQ05rAF9IAHYFVzEqQIZJghATQiOfH60D6YI6eT3r8yBr10YU6ILd4DXug==
-::eg0/rx1wNQPfEVWB+kM9LVsJDC2ML3+7Crlc/eby/P6GrkFdcuQyaIrcmoCBL+xd6ETwFQ==
-::fBEirQZwNQPfEVWB+kM9LVsJDC2ML3+7Crlc/eby/P6GrkFdcuQyaIrcmoCBL+xz
-::cRolqwZ3JBvQF1fEqQIZJghATQiOfH60D6YI6eT3r8yBr10YU6ILd4DXug==
-::dhA7uBVwLU+EWHellA==
-::YQ03rBFzNR3SWATE2mNQ
-::dhAmsQZ3MwfNWATE2mNQ
-::ZQ0/vhVqMQ3MEVWAtB9wBjo0
-::Zg8zqx1/OA3MEVWAtB9wBjo0
-::dhA7pRFwIByZRRmq1SI=
-::Zh4grVQjdCqDJGmW+0UiKRYUag2OOXiGKbsZzPry+e+7hkIeQe86dpvI5pOHNMgG6HnGdIIu3lxTlM4wJRVLahOnYk8hp2dRomWJO4e6tQLxT0/J0kQ/F1ldiXDEjSc3IMZmk9AX0iyyvGv5krUV1jjLUaYCW2b5xMw=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 setlocal enabledelayedexpansion
 title Install/unistall Ffmpeg Tool
